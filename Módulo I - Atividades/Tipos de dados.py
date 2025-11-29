837d4392b1f4732b3d7641670a755a6cfe583cbf
JordandMarco <jhordan.santos100@gmail.com>
2025-11-28 21:46:08 -0300

    Modulo I - Concluido

---
+++ Módulo I - Atividades/Tipos de dados.py
@@ -0,0 +1,39 @@
+# Numero inteiro
+
+idade = 30
+print("Idade:", idade)
+
+# Tipo Float
+
+altura = 1.75
+print("Altura:", altura)
+
+# Tipo String
+
+nome = "Jordan de Marco"
+print("Olá, meu nome é", nome)
+
+# Tipo Booleano
+
+estudando_python = True
+print("Estudando Python:", estudando_python)
+
+# Tipo Lista
+
+frutas = ["maçã", "banana", "laranja"]
+print("Segunda fruta da lista:", frutas[1])
+
+# Tipo Tupla
+
+cores = ("vermelho", "azul", "verde", "amarelo")
+print("Primeira cor da tupla:", cores[0])
+
+# Tipo Dicionario
+
+aluno = {"nome": "João", "idade": 20, "nota": 9.5}
+print("Nota do aluno:", aluno["nota"])
+
+# Misturando tipos
+
+dados = [42, 3.14, "Python", False]
+print("Todos os dados da lista", dados)
